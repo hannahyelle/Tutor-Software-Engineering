f7af489f0f2e146bce8c696b0c040f5cc77eae46
KBendo98 <kleitionbendo98@gmail.com>
2021-03-13 15:34:54 +0100

    Initial toolbar design

---
+++ toolbar/app.js
@@ -0,0 +1,26 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navs');
+    const navLinks = document.querySelectorAll('.navs li');
+
+    burger.addEventListener('click', ()=>{
+        //Toggle Burger
+        nav.classList.toggle('nav-active');
+
+        //Animate Navs
+        navLinks.forEach((link, index) => {
+        if(link.style.animation){
+            link.style.animation = '';
+        }
+        else{
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+        }
+
+        });
+        
+        //Burger Animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
